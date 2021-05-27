200663ed746de8451844210bf130014f7bb0c5c0
Prince-love <84893465+Prince-love@users.noreply.github.com>
2021-05-27 14:38:33 +0800

    Project
    initial commit

---
+++ nav.php
@@ -0,0 +1,12 @@
+<h1>Police Emergency ervice System</h1>
+
+<nav id="HeaderStyle">
+	
+	<a href="logical.php">Log Call</a>
+	
+	<a href="Update.php">Update</a>
+	
+	<a href="#">Report</a>
+	
+	<a href="#">History</a>
+	</nav>
